8236175009e906bb413f75331826a87ee06ea935
nheumann <nastassia.heumann@vw-dilab.com>
2021-07-06 13:24:54 +0200

    adds grouping and printing result

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -29,11 +29,36 @@ object Sindy {
     val attributeGroups = df.groupBy("value").agg(collect_set(col("column_name")).as("column_names"))
       .select("column_names")
       .distinct()
+      .as[Seq[String]]
+      /*
+      .select("column_names")
+      .distinct()
       .collect().map(_(0)).toList
+      */
+    println(attributeGroups)
+
+    val indCandidates = attributeGroups
+      .select(explode(attributeGroups("column_names")).as("key"), col("column_names"))
+      .as[(String, Seq[String])]
+      .map(c => (c._1, c._2.filter(_ != c._1)) )
+      .toDF("lhs", "rhs")
+    indCandidates.show()
+
+    val groupedIndCandidates = indCandidates
+      .groupBy(indCandidates("lhs")).agg(collect_set(indCandidates("rhs")))
+      .as[(String,Seq[Seq[String]])]
+      .map(c => (c._1,c._2.reduce(_.intersect(_))))
+      .filter(c=> c._2.nonEmpty)
+
+    groupedIndCandidates.show()
+
+    groupedIndCandidates.foreach(c => println(c._1 + " <= " + c._2.reduce(_ + ", " + _)) )
 
 
-    print(attributeGroups)
-    // val indCandidates = attributeGroups.
+    // val test1 = attributeGroups.map(arr => explode(arr))
+    // println(test1)
+
+    // val indCandidates = attributeGroups.map(group => group.map(attribute => (attribute, )))
 
 
 
