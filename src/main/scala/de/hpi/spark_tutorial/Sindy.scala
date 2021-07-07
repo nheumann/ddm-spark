59acadde700fed4fad675e03a93d8274cc5dc75d
nheumann <nastassia.heumann@vw-dilab.com>
2021-07-07 11:21:49 +0200

    cleans up sindy code and and adds slides

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -3,6 +3,7 @@ package de.hpi.spark_tutorial
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
+
 object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
@@ -15,62 +16,39 @@ object Sindy {
         .option("delimiter", ";")
         .csv(file)
     })
-    println(datasets.length)
 
+    // create cells of datasets
     val cells = datasets.flatMap(data => data.columns.map(data.select(_)))
       .map(col => col
       .map(row => (row.get(0).toString, row.schema.fieldNames.head)))
 
-    val df = cells.reduce(_ union _)
-      .toDF("value", "column_name")
-    df.show(false)
-
     // joining of attribute groups
-    val attributeGroups = df.groupBy("value").agg(collect_set(col("column_name")).as("column_names"))
+    val attributeGroups = cells.reduce(_ union _)
+      .toDF("value", "column_name")
+      .groupBy("value").agg(collect_set(col("column_name")).as("column_names"))
       .select("column_names")
       .distinct()
       .as[Seq[String]]
-      /*
-      .select("column_names")
-      .distinct()
-      .collect().map(_(0)).toList
-      */
-    println(attributeGroups)
 
+    // compute all ind candidates from attribute groups
     val indCandidates = attributeGroups
       .select(explode(attributeGroups("column_names")).as("key"), col("column_names"))
       .as[(String, Seq[String])]
       .map(c => (c._1, c._2.filter(_ != c._1)) )
       .toDF("lhs", "rhs")
-    indCandidates.show()
 
+    // group and sort all ind candidates by left hand side
     val groupedIndCandidates = indCandidates
       .groupBy(indCandidates("lhs")).agg(collect_set(indCandidates("rhs")))
       .as[(String,Seq[Seq[String]])]
       .map(c => (c._1,c._2.reduce(_.intersect(_))))
       .filter(c=> c._2.nonEmpty)
+      .toDF("lhs", "rhs")             
+      .sort("lhs")
+      .as[(String,Seq[String])]
 
-    groupedIndCandidates.show()
-
-    groupedIndCandidates.foreach(c => println(c._1 + " <= " + c._2.reduce(_ + ", " + _)) )
-
-
-    // val test1 = attributeGroups.map(arr => explode(arr))
-    // println(test1)
-
-    // val indCandidates = attributeGroups.map(group => group.map(attribute => (attribute, )))
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // print the inds
+    groupedIndCandidates.foreach(c => println(c._1 + " < " + c._2.reduce(_ + ", " + _)) )
   }
+
 }
